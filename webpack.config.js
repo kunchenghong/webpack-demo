168567500abc268ad317540054060edec38a7dd4
yulinni <hkc.hkc@bitsun-inc.com>
2023-05-08 19:42:59 +0800

    webpack-dev-server需要固定一个版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,27 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: './src/index.html',
+      filename: 'app.html',
+      inject: 'body'
+    })
+  ],
+  mode: 'development',
+  // 设定源代码为原来样式
+  devtool: 'inline-source-map',
+
+  // npx webpack --watch  文件更新后代码自动编译
+
+  devServer: {
+    static: './dist'
   }
 }
